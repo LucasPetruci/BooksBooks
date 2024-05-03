6b071cc1379aba5b711eb0b968cf3a9eee12ad01
LucasPetruci <lucaspetruci100@gmail.com>
2024-05-03 15:18:52 -0300

    feat: add login screen

---
+++ src/pages/LoginScreen.tsx
@@ -0,0 +1,99 @@
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  Button,
+} from "react-native";
+import React, { useState } from "react";
+import { auth } from "../firebase/Firebase-config";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+export function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const authen = auth;
+
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(
+        authen,
+        email,
+        password
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+      alert("Sign in failed: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signUp = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        authen,
+        email,
+        password
+      );
+      console.log(response);
+      alert("check email");
+    } catch (error: any) {
+      console.log(error);
+      alert("" + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Loginn</Text>
+      <TextInput
+        value={email}
+        style={styles.input}
+        placeholder="Email"
+        autoCapitalize="none"
+        onChangeText={(text) => setEmail(text)}
+      ></TextInput>
+      <TextInput
+        value={password}
+        style={styles.input}
+        placeholder="Password"
+        autoCapitalize="none"
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+      ></TextInput>
+
+      {loading ? (
+        <ActivityIndicator size="large" color="#fff"></ActivityIndicator>
+      ) : (
+        <>
+          <Button title="Login" onPress={() => signIn()}></Button>
+          <Button title="Create account" onPress={() => signUp()}></Button>
+        </>
+      )}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#f8f8f8",
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  input: {
+    borderColor: "black",
+  },
+});
