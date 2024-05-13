d7d22c05bdc34fdfc40ed37d6c98334481808b86
LucasPetruci <lucaspetruci100@gmail.com>
2024-05-13 17:58:09 -0300

    feat: select author

--- src/components/postBook.tsx
+++ src/components/postBook.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { db } from "../firebase/Firebase-config";
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase/Firebase-config";
 import {
   SafeAreaView,
   View,
@@ -7,16 +7,47 @@ import {
   Button,
   TouchableOpacity,
   Text,
+  FlatList,
+  Pressable,
+  StyleSheet,
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { getDatabase, ref, get } from "firebase/database";
+import { getAuthors } from "./getAuthor";
 
 const PostBook = () => {
+  const [authors, setAuthors] = useState([]);
   const [title, setTitle] = useState("");
   const [summary, setSummary] = useState("");
   const [authorId, setAuthorId] = useState("");
   const [pubDate, setPubDate] = useState<Date | null>(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const [selectedAuthorsId, setSelectAuthorsId] = useState([]);
+
+  useEffect(() => {
+    async function fetchAuthors() {
+      const authorsData = await getAuthors();
+      if (authorsData) {
+        setAuthors(authorsData);
+      }
+    }
+
+    fetchAuthors();
+  }, []);
+
+  function handleAuthorListPress(newAuthorId: any, element: any) {
+    if (selectedAuthorsId.includes(newAuthorId)) {
+      setSelectAuthorsId(
+        selectedAuthorsId.filter((author) => author !== newAuthorId)
+      );
+      console.log(selectedAuthorsId);
+    } else {
+      setSelectAuthorsId((xxx: Array<any>) => [...xxx, newAuthorId]);
+      element.style = styles.selectedAuthor;
+      console.log(selectedAuthorsId);
+    }
+  }
 
   const handlePost = async () => {
     if (title === "" || summary === "" || authorId === "") {
@@ -49,9 +80,28 @@ const PostBook = () => {
           placeholder="Book Summary: "
           onChangeText={(text) => setSummary(text)}
         />
-        <TextInput
-          placeholder="Author name: "
-          onChangeText={(text) => setAuthorId(text)}
+        <Text>Book author:</Text>
+        {selectedAuthorsId.map((author) => {
+          return author.name;
+        })}
+        <FlatList
+          data={authors}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <Pressable
+              style={[
+                styles.author,
+                selectedAuthorsId.includes(item.id)
+                  ? styles.selectedAuthor
+                  : {},
+              ]}
+              onPress={(e) => {
+                handleAuthorListPress(item.id, e.target);
+              }}
+            >
+              <Text>{item.data.name}</Text>
+            </Pressable>
+          )}
         />
         <TouchableOpacity onPress={() => setShowDatePicker(true)}>
           <Text>{pubDate ? pubDate.toDateString() : "Select Date"}</Text>
@@ -61,5 +111,15 @@ const PostBook = () => {
     </SafeAreaView>
   );
 };
-
+const styles = StyleSheet.create({
+  author: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+  },
+  selectedAuthor: {
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    backgroundColor: "#000000aa",
+  },
+});
 export default PostBook;
