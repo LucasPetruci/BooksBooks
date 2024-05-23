fa5330e919c99f7c47e4cd96b9a417f7fac557bf
LucasPetruci <lucaspetruci100@gmail.com>
2024-05-23 18:35:07 -0300

    fix: split login/register from main Routes
    Co-authored-by: João Victor Capello <Pinais@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { Routes } from "./src/pages/routes";
+import { Login_Register, Routes } from "./src/pages/routes";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const RootStack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes></Routes>
+      <RootStack.Navigator>
+        <RootStack.Screen
+          name="Root"
+          component={Routes}
+          options={{ headerShown: false }}
+        ></RootStack.Screen>
+        <RootStack.Screen
+          name="Login"
+          component={Login_Register}
+          options={{ headerShown: false }}
+        ></RootStack.Screen>
+      </RootStack.Navigator>
     </NavigationContainer>
   );
 }
--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,30 +2,35 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Login } from "./LoginScreen";
 import { Profile } from "./ProfileScreen";
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { withAuthentication } from "../components/withAuthentication";
 import { AuthorCreation } from "./AuthorCreationScreen";
 import { BookCreation } from "./BookCreationScreen";
 
 import { Post } from "./PostScreen";
 import { ForYou } from "./ForYouScreen";
+import { Register } from "./RegisterScreen";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Tab = createBottomTabNavigator();
-
+const Stack = createStackNavigator();
 const AuthenticatedProfile = withAuthentication(Profile);
 const AuthenticatedPost = withAuthentication(Post);
 const AuthenticatedAuthorCreation = withAuthentication(AuthorCreation);
 const AuthenticatedBookCreation = withAuthentication(BookCreation);
 
+export function Login_Register() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}></Stack.Screen>
+      <Stack.Screen name="Register" component={Register}></Stack.Screen>
+    </Stack.Navigator>
+  );
+}
+
 export function Routes() {
   return (
     <Tab.Navigator initialRouteName="Foryou">
-      <Tab.Screen
-        name="Login"
-        component={Login}
-        options={{ headerShown: false }}
-      ></Tab.Screen>
-
       <Tab.Screen name="Foryou" component={ForYou}></Tab.Screen>
 
       <Tab.Screen
@@ -35,7 +40,7 @@ export function Routes() {
 
       <Tab.Screen name="Post" component={AuthenticatedPost}></Tab.Screen>
 
-      <Tab.Screen name="Profile" component={AuthenticatedProfile}></Tab.Screen>
+      <Tab.Screen name="Profile" component={Profile}></Tab.Screen>
 
       <Tab.Screen
         name="AuthorCreation"
