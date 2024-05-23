e1b9510f2fd9d57d6083277918c3ec41694ab984
LucasPetruci <lucaspetruci100@gmail.com>
2024-05-23 18:40:47 -0300

    feat: select image and get name

--- src/pages/ProfileScreen.tsx
+++ src/pages/ProfileScreen.tsx
@@ -1,11 +1,43 @@
 import { View, Text, SafeAreaView, Button, Alert } from "react-native";
-import React from "react";
-import { auth } from "../firebase/Firebase-config";
+import React, { useEffect, useState } from "react";
+import { auth, db } from "../firebase/Firebase-config";
 import { useNavigation } from "@react-navigation/native";
 import { Routes } from "./routes";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { withAuthentication } from "../components/withAuthentication";
+import { onAuthStateChanged } from "firebase/auth";
+import { Login } from "./LoginScreen";
+import UploadImage from "../components/uploadImage";
+
+export const getUserData = async () => {
+  const q = query(
+    collection(db, "users"),
+    where("uid", "==", auth.currentUser.uid)
+  );
+  const querySnapshot = await getDocs(q);
+  let userData;
+  querySnapshot.forEach((doc) => {
+    userData = doc.data();
+    console.log(userData["name"]);
+  });
+  return userData;
+};
 
 export function Profile() {
+  const [userData, setUserData] = useState(null);
   const navigation = useNavigation();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    console.log("Profile screen mounted");
+    getUserData().then((data) => setUserData(data));
+
+    const unsub = onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+    });
+
+    return () => unsub();
+  }, []);
 
   const logout = () => {
     auth.signOut().then((response) => {
@@ -13,11 +45,17 @@ export function Profile() {
       navigation.navigate("Foryou");
     });
   };
-
   return (
     <SafeAreaView>
-      <Text>profile</Text>
-      <Button title="Logout" onPress={logout} />
+      <Text> {userData ? userData["name"] : "Loading..."} </Text>
+      {isAuthenticated ? (
+        <>
+          <UploadImage />
+          <Button title="Logout" onPress={logout} />
+        </>
+      ) : (
+        <Button title="Login" onPress={() => navigation.navigate("Login")} />
+      )}
     </SafeAreaView>
   );
 }
