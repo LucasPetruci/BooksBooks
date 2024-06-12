e202fe9cd504451e2ee4fb909454d7018efc0584
LucasPetruci <lucaspetruci100@gmail.com>
2024-06-11 23:02:00 -0300

    feat: foryou screen

---
+++ src/views/ForYouScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { GetForm } from "../components/getForm";
+
+export function ForYou() {
+  return (
+    <SafeAreaView>
+      <GetForm />
+    </SafeAreaView>
+  );
+}
