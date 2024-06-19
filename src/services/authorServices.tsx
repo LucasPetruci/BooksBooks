1a0385c41f6095d9ff34eeb77cf02fe5d0fdb1c4
LucasPetruci <lucaspetruci100@gmail.com>
2024-06-18 22:41:14 -0300

    fix (authorServices) : getAll, getById, getByName, create
    Co-authored-by: João Victor Capello <Pinais@users.noreply.github.com>

--- src/services/authorServices.tsx
+++ src/services/authorServices.tsx
@@ -1,13 +1,12 @@
-import { getFirestore, collection, getDocs, addDoc, updateDoc, doc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, addDoc, updateDoc, doc, getDoc } from "firebase/firestore";
 import { authorPublicDTO, authorInternalDTO } from "../dto/authorDTO";
 import { statusDTO } from "../dto/statusDTO";
 
-//TODO - Testar funçoes de authorServices no getForm.tsx
-export async function getAll(): Promise<Array<authorPublicDTO> | null> {
+//OK
+export async function getAllAuthors(): Promise<Array<authorPublicDTO> | null> {
   const db = getFirestore();
   const authorRef = collection(db, "/author");
   const authorSnapshot = await getDocs(authorRef);
-
   if (!authorSnapshot.empty) {
     const authorList = authorSnapshot.docs.map(
       (doc) =>
@@ -17,33 +16,34 @@ export async function getAll(): Promise<Array<authorPublicDTO> | null> {
   }
   return null;
 }
-
-export async function getById(id: string): Promise<authorPublicDTO | null> {
+//OK
+export async function getAuthorById(id: string): Promise<authorPublicDTO | null> {
   const db = getFirestore();
-  const authorRef = collection(db, "/author/" + id);
-  const authorSnapshot = await getDocs(authorRef);
+  const authorRef = doc(db, "/author/" + id );
+  const authorSnapshot = await getDoc(authorRef);
 
-  if (!authorSnapshot.empty) {
-    return {name: authorSnapshot.docs[0].data().name} as authorPublicDTO;
+  if (authorSnapshot.exists()) {
+    const data = authorSnapshot.data();
+    return {name: data.name} as authorPublicDTO;
   }
   return null;
 }
-export async function getByName(name: string): Promise<authorInternalDTO | null> {
+//OK
+export async function getAuthorByName(name: string): Promise<authorInternalDTO | null> {
   const db = getFirestore();
   const authorRef = collection(db, "/author");
   const authorSnapshot = await getDocs(authorRef);
-  authorSnapshot.docs.forEach(currAuthor => {
-    if (currAuthor.data().name === name){
-      return {
-        id:currAuthor.id,
-        name: currAuthor.id
-      };
-    }
-  });
+  const wanted = authorSnapshot.docs.find(currAuthor => currAuthor.data().name === name)
+  if (wanted){
+    return {
+      id: wanted.id,
+      name: wanted.data().name
+    };
+  }
   return null;
 }
-
-export async function create(name): Promise<authorPublicDTO | null> {
+//OK
+export async function createAuthor(name): Promise<authorPublicDTO | null> {
   const db = getFirestore();
   const authorRef = collection(db, "/author");
   const data = { name: name };
@@ -53,12 +53,16 @@ export async function create(name): Promise<authorPublicDTO | null> {
     name: data.name,
   };
 }
-export async function update(id: string, newName: string): Promise<statusDTO | null>{
+
+//TODO - testar essa função, TDD type
+export async function updateAuthor(id: string, newName: string): Promise<statusDTO | null>{
   const db = getFirestore();
   const authorRef = doc(db, "/author/"+id);
   await updateDoc(authorRef, {name: newName});
   return { status: 200, message: "Author updated"};
 }
-export async function remove() {
+
+//TODO - testar essa função, TDD type
+export async function removeAuthor() {
 
 }
